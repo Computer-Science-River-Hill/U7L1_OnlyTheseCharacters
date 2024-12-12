2d97b8dea31be3e3e9619216cb4e4248764acd51
chadboothe <43538127+chadboothe@users.noreply.github.com>
2024-12-12 09:36:23 -0500

    Update test_pytest.py

--- test_pytest.py
+++ test_pytest.py
@@ -1,13 +1,13 @@
 import main    # The code to test
 
-def test_One():
+def test_one():
     assert main.only([1,2,3,4,5],1,3) == False
 
-def test_Two():
+def test_two():
     assert main.only([1,4,1,4,1],1,4) == True
 
-def test_Three():
+def test_three():
     assert main.only([1,4,1,5,1],1,4) == False
 
-def test_Four():
+def test_four():
     assert main.only([2,8,8,2,8],2,8) == True
