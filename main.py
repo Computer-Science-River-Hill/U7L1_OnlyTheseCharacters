71da932e5755803bcff6d6e341fddd9bfdfa3266
chadboothe <43538127+chadboothe@users.noreply.github.com>
2024-12-12 14:32:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# Code Goes Here
+# Setup Code
+import module
+
+# User input to build your list
+
+# User input for 1st number to find
+
+# User input for 2nd number to find
+
+answer = module.only(myList, nbr1, nbr2)
+print(answer)
