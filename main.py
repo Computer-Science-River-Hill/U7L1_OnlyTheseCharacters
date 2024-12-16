745ac47677a30fdddc831f4eca02999d315da86f
chadboothe <43538127+chadboothe@users.noreply.github.com>
2024-12-16 19:10:23 +0000

    121624

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 import module
 
 # User input to build your list
+myList = []
+
+for x in range(5):
+    nbr = int(input("Number: "))
+    myList.append(nbr)
 
 # User input for 1st number to find
+nbr1 = int(input("Number to Find: "))
 
 # User input for 2nd number to find
+nbr2 = int(input("Number to Find: "))
 
 answer = module.only(myList, nbr1, nbr2)
 print(answer)
