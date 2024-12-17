e9b69acfe6e7261e94fe5811b07e2c3b7bcf05c2
chadboothe <43538127+chadboothe@users.noreply.github.com>
2024-12-17 10:08:59 -0500

    Update module.py

--- module.py
+++ module.py
@@ -1,8 +1,1 @@
 # Write your function here
-def only(myList, nbr1, nbr2):
-    for x in myList:
-        if x == nbr1 or x == nbr2:
-            pass
-        else:
-            return False
-    return True
